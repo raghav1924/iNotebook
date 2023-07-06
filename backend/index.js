755ca68ebd1a36c38212d75eaaae66b03f505624
jatinsharma <raghavjatin245@gmail.com>
2023-07-06 13:09:06 +0530

    Video 44 completed Schema made

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ connectToMongo();
 const app=express();
 const port=3000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello word');
-})
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+
 app.listen(port,()=>{
     console.log(`Example app listing to http://localhost:${port}`)
 })
